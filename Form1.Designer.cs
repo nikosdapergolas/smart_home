2ce1ba163ff14dcc7f44f52ffaaa7ad76be440f7
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2022-01-09 19:53:37 +0200

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Smart_home
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Smart_home.Properties.Resources.woman_assistant;
+            this.pictureBox1.Location = new System.Drawing.Point(241, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(394, 355);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Aqua;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(654, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "change avatar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Purple;
+            this.button2.Location = new System.Drawing.Point(263, 394);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(346, 118);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Check my home!";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(895, 582);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smart_home
+{
+    public partial class Form1 : Form
+    {
+        // A list that contains all the URLs of the pictures for the avatars
+        List<String> assistantAvatar = new List<string>();
+
+        // An integer that shows the current avatar in the list (by default it's the woman)
+        int currentAvatar = 0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Adding all the URLs of the assistant's pictures 
+            assistantAvatar.Add("pictures/woman_assistant.jpg");
+            assistantAvatar.Add("pictures/man_assistant.jpg");
+            assistantAvatar.Add("pictures/robot_assistant.jpg");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Checking if i have reached the end of the list
+            // If not, then i show the next avatar on the list
+            // else i show the first one
+            if(currentAvatar != (assistantAvatar.Count - 1))
+            {
+                currentAvatar++;
+                pictureBox1.ImageLocation = assistantAvatar[currentAvatar];
+            }
+            else
+            {
+                currentAvatar = 0;
+                pictureBox1.ImageLocation = assistantAvatar[currentAvatar];
+            }
+            
+
+
+            // Shows the number of items in the list 
+            //MessageBox.Show(assistantAvatar.Count.ToString());
+        }
+    }
+}
