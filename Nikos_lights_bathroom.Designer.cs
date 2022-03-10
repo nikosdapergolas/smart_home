2c51a265eeb86c4382261c8931ca501c57a7bf11
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2022-03-10 16:44:27 +0200

    not finished light yet

--- Nikos_lights_bathroom.Designer.cs
+++ Nikos_lights_bathroom.Designer.cs
@@ -132,7 +132,7 @@ namespace Smart_home
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = global::Smart_home.Properties.Resources.lightbulb2;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(3, 77);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(115, 113);
